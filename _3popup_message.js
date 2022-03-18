8b4d86ee4d117c3591a93a84074dbcb967fa769c
tushar239 <tushar239@yahoo.com>
2022-03-18 18:04:25 +0530

    first commit

--- _3popup_message.js
+++ _3popup_message.js
@@ -1,4 +1,4 @@
-alert("Wow, this is my own popup window");
+alert("Wow, this is my own popup window"); // just shows message
 
-var age = prompt("How old are you?");
+var age = prompt("How old are you?"); // asks a question and lets you enter the answer
 displayWithBr(age);
